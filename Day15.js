1ad7bbbafedb664ba45ea56ca54090f7481448a3
Abhishek <abhishekkashyap152002@gmail.com>
2026-03-27 16:53:32 +0530

    Day 15 - JS Variables and Functions

---
+++ Day15.js
@@ -0,0 +1,7 @@
+function mydata(name , age, mob) {
+return `My name is ${name} and my age  is ${age} and my mobile number is ${mob}`;
+}
+
+
+console.log(mydata("abhishek",23, 9389869959));
+console.log(mydata("Claude", 5, "Pta nahi kya hai"));
---
+++ jspractice.js
@@ -0,0 +1,152 @@
+// let age = 19;
+// if(age > 18){
+//     console.log("eligible for voting")
+
+// } else{
+//     console.log("not eligible for voting")
+// }
+
+/*
+ let num = prompt("enter the num");
+
+if(num%2 == 0){
+    console.log("num is even")
+}
+else{
+    console.log("num is odd")
+}
+ 
+*/
+
+/*let num = parseInt(prompt("Enter a number"));
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("Divisible by 3 and 5");
+} else if (num % 3 === 0) {
+  console.log("Divisible by 3");
+} else if (num % 5 === 0) {
+    console.log("Divisible by 5");
+} else {
+    console.log("Not divisible by 3 or 5")
+}
+*/
+
+/*let num = parseInt(prompt("Enter a number"));
+if(num%2===0 && num > 50){
+    console.log("Even and greater than 50");
+} else if(num%2===0 && num <50){
+    console.log("Even but small");
+} else if(num%2===1 && num > 50){
+    console.log("Odd and greater than 50")
+} else{
+    console.log("Odd but small")
+} */
+
+/* let num = 10;
+    while(num <= 200){
+        console.log(num);
+        num= num+10;
+    } */
+
+/* let num = 10;
+        while(num >=1) {
+            console.log(num);
+            num = num - 1;
+        } */
+/* let num = 105;
+while(num >= 7){
+    console.log(num);
+    num = num -7;
+} */
+
+   /* let num = 1;
+    let sum = 0;
+    while(num <= 10){
+        sum = sum+num;
+        num++;
+    }
+    console.log(sum);  */
+
+   /* let num = 1;
+    let sum = 0;
+    while(num <= 20){
+        if(num%2===0){
+            sum = sum+num;
+        } 
+
+        num++;
+    }
+    console.log(sum);
+*/
+
+/* let num = 1;
+while(num <= 5){
+    console.log(num*num);
+    num++;
+    
+
+} */
+
+
+/* let fact = 1;
+let n = 6;
+let i = 1;
+while(i <= n){
+    fact = fact*i;
+    i++;
+
+}
+
+    console.log("Factorial of " + n + " is: " + fact); */
+
+
+/* let a = 0;
+ let b = 1;
+ let i = 1;
+ let c;
+ while(i <= 10){
+   
+    console.log(a);
+    c = a+b;
+    a = b;
+    b = c;
+    i++;
+    
+ }    */
+
+/* let frontend = ["html","css","javascript", "bootstrap", "query" ]    
+
+
+
+console.log(frontend[0]); */
+
+/* let arr = [1,2,3,4,5];
+let newArr = [];
+
+for(let i = 0; i < arr.length ; i++){
+    newArr = arr[i]*arr[i]
+    console.log(newArr);
+} */
+
+ 
+   
+
+
+  /*   
+  let arr = [1,2,3,4,5];
+  let newArr = [];
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i]%2===0)
+            console.log(arr[i]);
+    }
+ */
+
+
+    let arr = [1,2,3,4,5];
+    let reversedArr = [];
+
+for(let i = arr.length-1; i >= 0 ; i--) {
+      reversedArr.push(arr[i]);
+}   
+
+console.log(reversedArr);
