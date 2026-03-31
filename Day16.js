7c4e66df7d2c18ee4e19af9a066657e40ffb10b9
Abhishek <abhishekkashyap152002@gmail.com>
2026-03-31 14:56:17 +0530

    Day 16 - Arrays Objects forEach map filter

--- Day16.js
+++ Day16.js
@@ -30,3 +30,26 @@ let filtergrocerylist = grocerylist.filter(function(cheez){
 return cheez.length > 5
 })
 console.log(filtergrocerylist)
+
+// Objects
+
+let mydetails = {
+    name: "Abhishek",
+    age: 23,
+    mob: 9389869959,
+    email: "Abhishekkashyap152002@gmail.com",
+    dob: `15-8-2003`,
+    city: "Meerut",
+    qualification: "Btech CSE",
+    Skills: ["Html", "CSS", "JS", "GIT","GITHUB"]
+}
+
+console.log(mydetails)
+console.log(mydetails.name)
+console.log(mydetails["dob"])
+console.log(mydetails.Skills[0])
+console.log(mydetails.city)
+
+mydetails.Skills.forEach(function(details){
+    console.log(details)
+})
