37ecb9bc1f28f11f957560595ce829e7c0654f16
Abhishek <abhishekkashyap152002@gmail.com>
2026-04-18 01:30:21 +0530

    Day 27: Error Handling complete

--- Day 24 Weather app/app.js
+++ Day 24 Weather app/app.js
@@ -1,4 +1,8 @@
-async function fetchweather() {
+ async function fetchweather() {
+   
+    let outputdata = document.getElementById("output");
+    try{
+   
     let city = document.getElementById('cityinput').value
     const apikey = "71159a79c54b5f51055d47286c5ac6a3"
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
@@ -6,10 +10,14 @@ async function fetchweather() {
 
     let response =  await fetch(url);
     let data = await response.json();
-    let outputdata = document.getElementById("output");
-    console.log(data)
+    
     outputdata.innerHTML = "City Name - " + data.name + " - " +" Temperature - " + data.main.temp;
+ }
 
-
+ catch( error){
+    outputdata.innerHTML = " Error: " + error.message
     
 }
+    
+}
+
---
+++ Day 27 Error Handling/app.js
@@ -0,0 +1,9 @@
+
+try{
+console.log(abhishek) }
+
+catch(error){
+   console.log("Error aaya:", error.message)
+   console.log(error.name )
+   console.log(error.stack)
+}
