0e871134f16ba17a5a6b8d41e338a1756f443fa5
Abhishek <abhishekkashyap152002@gmail.com>
2026-04-17 22:12:51 +0530

    Day 24: Weather App

--- Day 23 Async Await/app.js
+++ Day 23 Async Await/app.js
@@ -1,7 +1,6 @@
 async function fetchdata() {
   let response = await fetch(
-    "https://official-joke-api.appspot.com/random_joke",
-  );
+    "https://official-joke-api.appspot.com/random_joke");
   let data = await response.json();
   let outputdata = document.getElementById("output");
   outputdata.innerHTML = data.setup + " - " + data.punchline;
---
+++ Day 24 Weather app/app.js
@@ -0,0 +1,15 @@
+async function fetchweather() {
+    let city = document.getElementById('cityinput').value
+    const apikey = "71159a79c54b5f51055d47286c5ac6a3"
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`
+
+
+    let response =  await fetch(url);
+    let data = await response.json();
+    let outputdata = document.getElementById("output");
+    console.log(data)
+    outputdata.innerHTML = "City Name - " + data.name + " - " +" Temperature - " + data.main.temp;
+
+
+    
+}
