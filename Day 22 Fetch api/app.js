c6959409c0f02493732fa50e65ffe5926926f1b7
Abhishek <abhishekkashyap152002@gmail.com>
2026-04-17 18:09:41 +0530

    Day 22 - Fetch API

--- Day 22 Fetch api/app.js
+++ Day 22 Fetch api/app.js
@@ -3,6 +3,7 @@
 function fetchdata(){
     let outputdata = document.getElementById("output")
     fetch('https://official-joke-api.appspot.com/random_joke')
+
     .then(abhishek => abhishek.json())
         .then(data => outputdata.innerHTML = data.setup + " - " + data.punchline)
 }
