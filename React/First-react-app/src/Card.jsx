f1ff0e7e6ef7c5acff7d1a6a6e5936f4240bb45b
Abhishek <abhishekkashyap152002@gmail.com>
2026-06-21 12:54:09 +0530

    Updating Deleted Folders

--- React/First-react-app/src/Card.jsx
+++
@@ -1,9 +0,0 @@
-function Card(props){
-    return <div>
-        <h1>{props.name}</h1>
-        <p>{props.role}</p>
-    </div>
-
-}
-
-export default Card
--- Ui-Project/src/components/Center.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const Center = () => {
-  return (
-    <div></div>
-  )
-}
-
-export default Center
